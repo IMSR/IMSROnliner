6b52d8e184ed8508dc94573223ecd3dc3871dd90
Istom1n <ivan@istomin.im>
2014-03-19 23:34:53 +0400

    Backup on Disable

--- src/imsr/Istom1n/IMSROnliner.java
+++ src/imsr/Istom1n/IMSROnliner.java
@@ -4,25 +4,27 @@ import imsr.Istom1n.Commands.IMSROnlinerCommand;
 import imsr.Istom1n.DB.Maps;
 import imsr.Istom1n.Listeners.OnOff;
 
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class IMSROnliner extends JavaPlugin {
 	
 	private Maps db;
+	private CommandExecutor cmdex = new IMSROnlinerCommand();
 	
 	@Override
 	public void onEnable() {
 		getLogger().info("IMSROnliner v" + getDescription().getVersion() + " is Enabled");
 		
-		
 		db = new Maps();
 		
 		getServer().getPluginManager().registerEvents(new OnOff(db), this);
-		getCommand("imsronliner").setExecutor(new IMSROnlinerCommand());
+		getCommand("imsronliner").setExecutor(cmdex);
 	}
 	
 	@Override
 	public void onDisable() {
+		((IMSROnlinerCommand) cmdex).backupMySQL();
 		getLogger().info("IMSROnliner v" + getDescription().getVersion() + " is Disabled");
 	}
 	
